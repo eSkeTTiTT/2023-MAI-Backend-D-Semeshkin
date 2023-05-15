ee08b9c91c4ae5f0294af850eb0e83b1c2176186
DSemeshkin <DSemeshkin@IBS.RU>
2023-05-15 20:15:40 +0300

    Реализация LRUCache fix

--- hmw_1/cache.py
+++ hmw_1/cache.py
@@ -1,25 +1,23 @@
+from datetime import datetime
 from typing import Any
 
 class LRUCache:
     def __init__(self, capacity: int) -> None:
         self.capacity = capacity
         self.cache = {}
-        self.counter = 0
 
     def get(self, key: str) -> str | None:
         if key in self.cache:
-            self.cache[key][1] = self.counter
-            self.counter += 1
+            self.cache[key][1] = datetime.now()
             return self.cache[key][0]
         else:
             return None
 
     def set(self, key: str, value: str) -> None:
-        if len(self.cache) >= self.capacity:
+        self.cache[key] = [value, datetime.now()]
+        if len(self.cache) > self.capacity:
             oldest = min(self.cache, key=lambda k: self.cache[k][1])
             del self.cache[oldest]
-        self.cache[key] = [value, self.counter]
-        self.counter += 1
 
     def rem(self, key: str) -> None:
         if key in self.cache:
